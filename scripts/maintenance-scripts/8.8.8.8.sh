910792850e2b10147af6e341dc3e09d679ebf811
luke-h1 <luke.howsam@yahoo.com>
2020-06-08 18:49:21 +0100

    update log loc for 8.8.8.8.sh

--- scripts/maintenance-scripts/8.8.8.8.sh
+++ scripts/maintenance-scripts/8.8.8.8.sh
@@ -4,9 +4,9 @@ isinfile=$(cat /etc/resolv.conf | grep -c "nameserver 8.8.8.8")
 
 if [ $isinfile -eq 0 ];
 	then
-		echo "string not in file" >> /root/nameserver.log  
+		echo "string not in file" >> /root/log/nameserver.log  
 		echo "nameserver 8.8.8.8" >> /etc/resolv.conf
                 echo "nameserver 8.8.4.4" >> /etc/resolv.conf 
 	else
-		echo "string in file" >> /root/nameserver.log 
+		echo "string in file" >> /root/log/nameserver.log 
 fi 
