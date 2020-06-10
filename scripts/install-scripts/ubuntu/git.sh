76dc7c6ccb1d2289bcb063ff39439095c0fe3f19
luke-h1 <luke.howsam@yahoo.com>
2020-06-10 12:17:43 +0100

    added all scripts for ubuntu into one main script

--- scripts/install-scripts/ubuntu/git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-
-git clone https://github.com/luke-h1/scrimba-html-css 
-git clone https://github.com/luke-h1/dotfiles 
-git clone https://github.com/luke-h1/responsive-webdesign
-git clone https://github.com/luke-h1/navbar-animation-css 
-git clone https://github.com/luke-h1/div-span-button-box-css
-git clone https://github.com/luke-h1/xfce
