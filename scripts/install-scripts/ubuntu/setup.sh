f5244c0ce0af9f590f379b165aad35d6ace56135
luke-h1 <luke.howsam@yahoo.com>
2020-06-12 19:37:22 +0100

    update to setup.sh

--- scripts/install-scripts/ubuntu/setup.sh
+++ scripts/install-scripts/ubuntu/setup.sh
@@ -20,25 +20,7 @@ wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add
 sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
 sudo apt update
 sudo apt install code
-echo "configuring vs code extensions....." 
-code --install-extension PeterJausovec.vscode-docker \
-code --install-extension christian-kohler.npm-intellisense \
-code --install-extension nur.just-black \ 
-code --install-extension esbenp.prettier-vscode-4.7.0 \ 
-code --install-extension hookyqr.beautify-1.5.0 \ 
-code --install-extension dbaeumer.vscode-eslint-2.1.5 \ 
-code --install-extension christian-kohler.path-intellisense-1.4.2 \ 
-code --install-extension coenraads.bracket-pair-colorizer-1.0.61  \ 
-code --install-extension pkief.material-icon-theme-4.1.0 \ 
-code --install-extension ritwickdey.liveserver-5.6.1 \ 
-code --install-extension zignd.html-css-class-completion-1.19.0 \ 
-code --install-extension shakram02.bash-beautify-0.1.1 \ 
-code --install-extension michelemelluso.code-beautifier-2.3.3 \ 
-code --install-extension ionutvmi.path-autocomplete-1.13.6 \ 
-
-
 echo "VS code extensions now configured..."     
-
 echo "now configuring your github" 
 echo "enter your github username:"
 read user
@@ -47,7 +29,16 @@ echo "added your username to git config global"
 echo "enter your github email:"
 read email
 git config --global --replace-all user.email "$email"
+mkdir /srv/git 
+mkdir /srv/isos 
+mkdir /srv/courses 
+rm -rf /home/lukehowsam/Pictures  
+rm -rf /home/lukehowsam/Documents  
+rm -rf /home/lukehowsam/Public 
+rm -rf /home/lukehowsam/Templates 
+rm -rf /home/lukehowsam/Music
+rm -rf /home/lukehowsam/Videos 
+echo "script finished"
+echo "rebooting in 5 seconds" 
+telinit 6 
 
-
-
-echo "script finished ......."
