fa43f01652f811f3aa7b0d2a07fd39f30c0592ab
luke-h1 <luke.howsam@yahoo.com>
2020-06-13 10:04:46 +0100

    reorg and added failsafe for btmp.sh

---
+++ scripts/maintenance-scripts/btmp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ -d /root/log/ ]; 
+then
+
+last -f /var/log/btmp >> /root/log/login_attempts.txt
+
+elif 
+	[ ! -d /root/log ]; then
+	mkdir /root/log 
+	last -f /var/log/btmp >> /root/log/login_attempts.txt
+fi 
--- scripts/unused/btmp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-last -f /var/log/btmp >> /root/log/login_attempts.txt 
