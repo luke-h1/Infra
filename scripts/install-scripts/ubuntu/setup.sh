f826426ed2f3c6887c8bb6d542b4e44df46b49d2
luke-h1 <luke.howsam@yahoo.com>
2020-06-17 16:25:31 +0100

    update

--- scripts/install-scripts/ubuntu/setup.sh
+++ scripts/install-scripts/ubuntu/setup.sh
@@ -8,6 +8,7 @@ sudo apt-get update -y
 sudo apt-get install -y nodejs npm nvm 
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+bash --login 
 nvm install node
 nvm install 8.10.0
 nvm use 10.16.3
