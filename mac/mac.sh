43e69bd0d006921ed8c7438b0b3eb2a65802cd96
luke-h1 <luke.howsam@yahoo.com>
2020-06-26 10:15:13 +0100

    fix permission issues and general cleanup  🐧

--- mac/mac.sh
+++ mac/mac.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "starting the install script..." 
 xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
@@ -9,35 +8,31 @@ curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | 
 nvm install stable
 brew update 
 brew upgrade node 
-npm install -g lite-server eslint #install lite-server & eslint globally 
+npm install -g eslint 
 echo "now configuring your github username & email globally..."
-echo "" 
-vcecho ""
-echo "enter your github username:" 
+echo "===================enter your github username==============
+=================================================================
+=================================================================:" 
 read user 
 git config --global --replace-all user.name "$user" 
-echo "" 
-echo "" 
+
 echo "added your username to git config global" 
-echo "" 
-echo "" 
-echo "enter your github email" 
+
+echo "===================enter your github email==============
+=================================================================
+=================================================================:" 
 read email 
 git config --global --replace-all user.email "$email" 
 echo "configured your github name & email globally"
-sleep 3 
 brew cask install postman  
 mkdir /srv/dev  
 cd /srv/dev 
-echo "putting github repos in /srv/dev directory..." 
-sleep 3  
 git clone https://github.com/luke-h1/scrimba-html-css 
 git clone https://github.com/luke-h1/dotfiles 
 git clone https://github.com/luke-h1/responsive-webdesign
 git clone https://github.com/luke-h1/navbar-animation-css 
 git clone https://github.com/luke-h1/div-span-button-box-css
 git clone https://github.com/luke-h1/Automation
-git clone https://github.com/transmission/transmission-releases/raw/master/Transmission-3.00.dmg
 mkdir -p /srv/isos 
 mkdir -p /srv/courses
 mkdir -p /Users/lukehowsam/notes 
@@ -47,7 +42,7 @@ cd /opt
 brew install wget  
 brew install python 
 brew install bash
-echo "/usr/local/sbin" >> /etc/paths 
+sudo echo "/usr/local/sbin" >> /etc/paths 
 brew install python
 brew cask install iterm2 
 brew install vcprompt 
--- scripts/mac/auth-fail.sh
+++ scripts/mac/auth-fail.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-
 log stream --predicate '(eventMessage CONTAINS "Authentication failed")' --style syslog
-Filtering the log data using "composedMessage CONTAINS "Authentication failed""
+Filtering the log data using "composedMessage CONTAINS "Authentication failed"" >> /root/auth-log.txt
