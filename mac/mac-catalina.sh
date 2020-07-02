39e87f091678f84ce815ee321b99dcec005df53a
Luke H <55030296+luke-h1@users.noreply.github.com>
2020-07-02 12:44:37 +0100

    Update mac-catalina.sh

--- mac/mac-catalina.sh
+++ mac/mac-catalina.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-xcode-select --install
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" < /dev/null 2> /dev/null ; brew install caskroom/cask/brew-cask 2> /dev/null
 brew update
