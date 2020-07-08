c64e52c5917c5d1236a7192db0a92497b5b557d5
luke-h1 <luke.howsam@yahoo.com>
2020-07-08 16:15:47 +0100

    fix merge conflicts

---
+++ mac/scripts/github.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+echo "Enter your github username:"
+read user 
+git config --global --replace-all user.name "$user" 
+echo "Enter your github email:"
+read email 
+git config --global --replace-all user.email "$email" 
+echo "Your github information has now been configured globally.."
