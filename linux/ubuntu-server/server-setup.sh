48c5f661788823022d8ba8b25901abf469c745f5
luke-h1 <luke.howsam@yahoo.com>
2020-07-11 15:50:58 +0100

    .

--- linux/ubuntu-server/server-setup.sh
+++ linux/ubuntu-server/server-setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 sudo apt-get install -y openssh* 
 wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb 
-sudo apt install ./teamviewer_amd64.deb
+sudo apt install ./teamviewer_amd64.deb 
+echo "-A ufw-before-input -p icmp --icmp-type echo-request -j DROP" >> /etc/ufw/before.rules
+sudo ufw disable
+sudo ufw enable
