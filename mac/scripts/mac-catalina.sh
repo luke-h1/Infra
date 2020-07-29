2787ddb3ff3b6b7ec92591bc376c2fcc36e5c505
luke-h1 <luke.howsam@yahoo.com>
2020-07-29 13:58:54 +0100

    further improvements

--- mac/scripts/mac-catalina.sh
+++ mac/scripts/mac-catalina.sh
@@ -96,17 +96,178 @@ defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
 
 
 echo ""
+echo "Would you like to set your computer name (as done via System Preferences >> Sharing)?  (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  echo "What would you like it to be?"
+  read COMPUTER_NAME
+  sudo scutil --set ComputerName $COMPUTER_NAME
+  sudo scutil --set HostName $COMPUTER_NAME
+  sudo scutil --set LocalHostName $COMPUTER_NAME
+  sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
+fi
+
+echo ""
+echo "Turn off keyboard illumination when computer is not used for 5 minutes"
+defaults write com.apple.BezelServices kDimTime -int 300
+
+echo ""
+echo "Disable display from automatically adjusting brightness? (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  sudo defaults write /Library/Preferences/com.apple.iokit.AmbientLightSensor "Automatic Display Enabled" -bool false
+fi
+
+echo ""
+echo "Where do you want screenshots to be stored? (hit ENTER if you want ~/Desktop as default)"
+# Thanks https://github.com/omgmog
+read screenshot_location
+echo ""
+
+f [ -z "${screenshot_location}" ]
+then
+  # If nothing specified, we default to ~/Desktop
+  screenshot_location="${HOME}/Desktop"
+else
+  # Otherwise we use input
+  if [[ "${screenshot_location:0:1}" != "/" ]]
+  then
+    # If input doesn't start with /, assume it's relative to home
+    screenshot_location="${HOME}/${screenshot_location}"
+  fi
+fi
+echo "Setting location to ${screenshot_location}"
+defaults write com.apple.screencapture location -string "${screenshot_location}"
+
+
+echo ""
+echo "What format should screenshots be saved as? (hit ENTER for PNG, options: BMP, GIF, JPG, PDF, TIFF) "
+read screenshot_format
+if [ -z "$1" ]
+then
+  echo ""
+  echo "Setting screenshot format to PNG"
+  defaults write com.apple.screencapture type -string "png"
+else
+  echo ""
+  echo "Setting screenshot format to $screenshot_format"
+  defaults write com.apple.screencapture type -string "$screenshot_format"
+fi
+
+
+echo ""
+echo "Show hidden files in Finder by default? (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  defaults write com.apple.Finder AppleShowAllFiles -bool true
+fi
+
+
+###############################################################################
+# Chrome, Safari, & WebKit
+###############################################################################
+
+echo ""
+echo "Privacy: Don't send search queries to Apple"
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
+echo ""
+echo "Hiding Safari's bookmarks bar by default"
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+echo ""
+echo "Hiding Safari's sidebar in Top Sites"
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+
+echo ""
+echo "Disabling Safari's thumbnail cache for History and Top Sites"
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+echo ""
+echo "Enabling Safari's debug menu"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+echo ""
+echo "Making Safari's search banners default to Contains instead of Starts With"
+defaults write com.apple.Safari FindOnPageMatchesWordStartsOnly -bool false
+
+echo ""
+echo "Removing useless icons from Safari's bookmarks bar"
+defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+
+#########################################################################
+# Transmission.app                                                            #
+###############################################################################
+
+
+echo ""
+echo "Do you use Transmission for torrenting? (y/n)"
+read -r response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  mkdir -p ~/Downloads/Incomplete
+
+  echo ""
+  echo "Setting up an incomplete downloads folder in Downloads"
+  defaults write org.m0k.transmission UseIncompleteDownloadFolder -bool true
+  defaults write org.m0k.transmission IncompleteDownloadFolder -string "${HOME}/Downloads/Incomplete"
+
+  echo ""
+  echo "Setting auto-add folder to be Downloads"
+  defaults write org.m0k.transmission AutoImportDirectory -string "${HOME}/Downloads"
+
+  echo ""
+  echo "Don't prompt for confirmation before downloading"
+  defaults write org.m0k.transmission DownloadAsk -bool false
+
+  echo ""
+  echo "Trash original torrent files after adding them"
+  defaults write org.m0k.transmission DeleteOriginalTorrent -bool true
+
+  echo ""
+  echo "Hiding the donate message"
+  defaults write org.m0k.transmission WarningDonate -bool false
+
+  echo ""
+  echo "Hiding the legal disclaimer"
+  defaults write org.m0k.transmission WarningLegal -bool false
+
+  echo ""
+  echo "Auto-resizing the window to fit transfers"
+  defaults write org.m0k.transmission AutoSize -bool true
+
+  echo ""
+  echo "Auto updating to betas"
+  defaults write org.m0k.transmission AutoUpdateBeta -bool true
+
+  echo ""
+  echo "Setting up the best block list"
+  defaults write org.m0k.transmission EncryptionRequire -bool true
+  defaults write org.m0k.transmission BlocklistAutoUpdate -bool true
+  defaults write org.m0k.transmission BlocklistNew -bool true
+  defaults write org.m0k.transmission BlocklistURL -string "http://john.bitsurge.net/public/biglist.p2p.gz"
+fi
+
+
+###############################################################################
+# Kill affected applications
+###############################################################################
+
+echo ""
 cecho "Done!" $cyan
 echo ""
 echo ""
-echo "################################################################################" $white
+cecho "################################################################################" $white
 echo ""
 echo ""
-echo "Note that some of these changes require a logout/restart to take effect." $red
+cecho "Note that some of these changes require a logout/restart to take effect." $red
+cecho "Killing some open applications in order to take effect." $red
 echo ""
-echo ""
-echo -n "Check for and install available OSX updates, install, and automatically restart? (y/n)? "
-read response
-if [ "$response" != "${response#[Yy]}" ] ;then
-    softwareupdate -i -a --restart
-fi
+
+find ~/Library/Application\ Support/Dock -name "*.db" -maxdepth 1 -delete
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+  "Dock" "Finder" "Mail" "Messages" "Safari" "SystemUIServer" \
+  "Terminal" "Transmission"; do
+  killall "${app}" > /dev/null 2>&1
+done
