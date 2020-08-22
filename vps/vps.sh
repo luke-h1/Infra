dbd342054b8e10b123488546caa90a6c0c8f780c
luke-h1 <luke.howsam@yahoo.com>
2020-08-22 13:38:23 +0100

    add vps setup

---
+++ vps/vps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+curl -sSL https://get.docker.com | sh
+curl -L https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+systemctl enable docker-compose-app
