4a43cda2c31983bd8dc2fdef9ec2a83b1f5748db
luke-h1 <luke.howsam@yahoo.com>
2020-09-15 18:53:33 +0100

    add vscode.sh shell script

---
+++ macbook/vscode.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# install all vs code extensions 
+code --install christian-kohler.npm-intellisense
+code --install nur.just-black
+code --install esbenp.prettier-vscode
+code --install dbaeumer.vscode-eslint
+code --install wix.vscode-import-cost
+code --install christian-kohler.path-intellisense
+code --install coenraads.bracket-pair-colorizer
+code --install pkief.material-icon-theme
+code --install ritwickdey.liveserver
+code --install zignd.html-css-class-completion
+code --install ritwickdey.live-sass
+code --install dsznajder.es7-react-js-snippets
+code --install octref.vetur
+code --install donjayamanne.githistory
+code --install vuetifyjs.vuetify-vscode
+code --install burkeholland.simple-react-snippets
+code --install equimper.react-native-react-redux
+code --install awandarajbir.react-vscode-extension-pack
+code --install igorming.useful-react-snippets
+code --install formulahendry.auto-rename-tag
+code --install mikestead.dotenv
+code --install vivainio.robotframework
+code --install jacobphilip.danfoss-robotframework-debug
+code --install jacobphilip.robotframeworklinter
+code --install keith.robotframework
+code --install tomiturtiainen.rf-intellisense
+code --install snooz82.rf-intellisense
+
