d95623669c29f8aac251aec2a8ce3417d84f6978
luke-h1 <luke.howsam@yahoo.com>
2020-10-04 13:29:53 +0100

    update php config

--- macbook/mac-catalina.sh
+++ macbook/mac-catalina.sh
@@ -362,11 +362,14 @@ sudo chmod 770 /usr/local/bin/geckodriver
 ###############################################################################
 # PHP SETUP  
 ###############################################################################
+cd /Users/lukehowsam/ 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '795f976fe0ebd8b75f26a6dd68f78fd3453ce79f32ecb33e7fd087d39bfeb978342fb73ac986cd4f54edd0dc902601dc') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-wget https://xamppguide.com/download/7-4-6-for-mac-osx/?wpdmdl=1209&amp;refresh=5f343e14226b91597259284 
+sudo mv composer.phar /usr/local/bin/composer
+wget https://xamppguide.com/download/7-4-6-for-mac-osx/?wpdmdl=1209&amp;refresh=5f343e14226b91597259284
+
 ###############################################################################
 # SET SHELL TO ZSH 
 ###############################################################################
