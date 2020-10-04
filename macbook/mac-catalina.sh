37df8b72bf41c34b6b59d842b061f13a1fe0cf9b
luke-h1 <luke.howsam@yahoo.com>
2020-10-04 13:25:28 +0100

    update php config

--- macbook/mac-catalina.sh
+++ macbook/mac-catalina.sh
@@ -366,7 +366,7 @@ php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === '795f976fe0ebd8b75f26a6dd68f78fd3453ce79f32ecb33e7fd087d39bfeb978342fb73ac986cd4f54edd0dc902601dc') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
-wget https://downloads.mamp.info/MAMP-PRO/releases/5.7/MAMP_MAMP_PRO_5.7.pkg 
+wget https://xamppguide.com/download/7-4-6-for-mac-osx/?wpdmdl=1209&amp;refresh=5f343e14226b91597259284 
 ###############################################################################
 # SET SHELL TO ZSH 
 ###############################################################################
