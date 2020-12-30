e68cc2bc033620b44dba61d23391b14c6b40b858
luke-h1 <luke.howsam@yahoo.com>
2020-12-30 13:48:39 +0000

    move cask installs to cash.sh since brew cask is broken

---
+++ macbook/cask.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+brew install --cask pycharm
+brew install --cask docker-toolbox
+brew install --cask postman
+brew install --cask iterm2
+brew install --cask vlc
+brew install --cask spectacle
