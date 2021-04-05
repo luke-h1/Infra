dfe4e583465b4199dd34e2730676b6634124985f
luke-h1 <luke.howsam@yahoo.com>
2021-04-05 11:56:34 +0100

    add setup-go.sh

---
+++ macbook/setup-go.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# after installing go...
+
+which go 
+go version 
+echo $PATH 
+echo $GOPATH
+cd /Users/lukehowsam
+mkdir go && cd go 
+mkdir src && cd src 
+# clone your repos here...
