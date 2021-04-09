58146ab73ed8d561908860162eb41f53e4798e95
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2021-04-09 18:54:49 +0100

    Update ubuntu.sh

--- archive/linux/ubuntu.sh
+++ archive/linux/ubuntu.sh
@@ -161,6 +161,15 @@ sudo systemctl status redis
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.8
+sudo apt-get update -y 
+sudo apt install software-properties-common -y 
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt install python3.8
+python3.8 --version
+sudo apt-get update -y 
+sudo apt-get install -y python3-pip 
+pip3 --version 
+sudo -H pip3 install pipenv
 #https://software.opensuse.org/download.html?project=shells%3Azsh-users%3Azsh-completions&package=zsh-completions
 sudo su - lhowsam
 sudo curl \
