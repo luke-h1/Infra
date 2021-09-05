5651baa739294995abf08a733e8d1bcc7413efcd
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2021-09-05 12:45:17 +0100

    Update vscode.sh

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -47,3 +47,5 @@ code --install-extension kamikillerto.vscode-colorize
 code --install-extension abusaidm.html-snippets
 code --install-extension ecmel.vscode-html-css
 code --install-extension eamodio.gitlens
+code --install-extension felixfbecker.php-pack
+code --install-extension bmewburn.vscode-intelephense-client
