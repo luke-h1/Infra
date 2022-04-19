20641008cfd4fb14f717ad939c8ee05390fb9260
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2022-04-19 09:54:33 +0100

    Update clean-docker.sh

--- docker/clean-docker.sh
+++ docker/clean-docker.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-docker system prune -a
-docker image prune
-docker rmi $(docker images -a -q)
-docker rm $(docker ps -a -f status=exited -q)
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
-docker volume prune
+docker system prune -a && docker image prune && docker rmi $(docker images -a -q) && docker rm $(docker ps -a -f status=exited -q) && docker stop $(docker ps -a -q) && docker rm $(docker ps -a -q) && docker volume prune
