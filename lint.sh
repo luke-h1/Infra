3dd957806086ae687c57b611cc07e5b7316c413e
Luke Howsam <55030296+luke-h1@users.noreply.github.com>
2022-08-21 20:55:19 +0100

    Delete lint.sh

--- lint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-shellcheck mac/bin/strap.sh
-shellcheck mac/clean-node.sh
-shellcheck mac/composer.sh
-shellcheck mac/nvm.sh
-shellcheck mac/re-identify-keyboard.sh
-shellcheck vscode/vscode.sh
