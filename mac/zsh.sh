6dec628051507437d4d6908457ae548c982c8f97
luke-h1 <luke.howsam@yahoo.com>
2022-09-03 19:53:54 +0100

    zsh

--- mac/zsh.sh
+++ mac/zsh.sh
@@ -1,6 +1,2 @@
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-
-
-# add before source oh-my-zsh.sh
-# fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src
-# rm -f ~/.zcompdump; compinit
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm -f ~/.zcompdump; compinit
