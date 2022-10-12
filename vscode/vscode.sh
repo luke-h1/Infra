429cf46b647ece646e24d3f059608ec0bef06c9f
luke-h1 <luke.howsam@yahoo.com>
2022-10-12 16:14:31 +0100

    update vscode.sh

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 # install all vs code extensions
 # make sure to have the 'code' command in your PATH 
-
 code --install-extension christian-kohler.npm-intellisense
 code --install-extension esbenp.prettier-vscode
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension wix.vscode-import-cost
 code --install-extension christian-kohler.path-intellisense
 code --install-extension zignd.html-css-class-completion
 code --install-extension formulahendry.auto-rename-tag
 code --install-extension mikestead.dotenv
 code --install-extension ms-python.python
 code --install-extension styled-components.vscode-styled-components
+code --install-extension orta.vscode-jest
 code --install-extension graphql.vscode-graphql
 code --install-extension kumar-harsh.graphql-for-vscode
 code --install-extension golang.go
@@ -25,7 +24,6 @@ code --install-extension bmewburn.vscode-intelephense-client
 code --install-extension eg2.vscode-npm-script
 code --install-extension sanity-io.vscode-sanity
 code --install-extension knisterpeter.vscode-commitizen
-code --install-extension GitHub.copilot
 code --install-extension Prisma.prisma
 code --install-extension TomiTurtiainen.rf-intellisense
 code --install-extension eamodio.gitlens
