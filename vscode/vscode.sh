e86eb55539fbe25ae3e5add97eaa9e404db7fd60
luke-h1 <luke.howsam@yahoo.com>
2022-12-14 18:28:33 +0000

    feat(vscode): update extensions

--- vscode/vscode.sh
+++ vscode/vscode.sh
@@ -24,3 +24,5 @@ code --install-extension knisterpeter.vscode-commitizen
 code --install-extension Prisma.prisma
 code --install-extension TomiTurtiainen.rf-intellisense
 code --install-extension eamodio.gitlens
+code --install-extension Gruntfuggly.todo-tree
+code --install-extension ms-vscode.vscode-js-profile-flame
