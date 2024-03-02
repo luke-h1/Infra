54568dfbfb1be82569ef0192020fd96a3473e6e3
luke-h1 <luke.howsam@yahoo.com>
2024-03-02 11:57:43 +0000

    chore - refactor folder structure

--- dokku/reclaim-docker-space.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Remove all unused containers, networks, images (both dangling and unreferenced), and optionally, volumes.
-
-# -a = Remove all unused images not just dangling ones
-
-sudo docker system prune -a
--- dokku/resize-ec2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt install cloud-guest-utils -y 
-sudo growpart /dev/xvda 16 # resize to 16gb
-sudo resize2fs /dev/xvda1
-df -h
-lsblk
